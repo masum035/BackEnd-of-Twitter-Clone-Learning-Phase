65b7abf6399bd6cbb4796d593fa13a1071359cf2
MasumBhai <abdullahmasum6035@gmail.com>
2022-11-04 19:30:21 +0600

    3 return type of controllers learnt

---
+++ Controllers/v1/ReturnTypeChekingController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TweeterBackend.Controllers.v1
+{
+    [EnableCors("Version01_CORS_Policy")]
+    [Route("[controller]")]
+    [BindProperties(SupportsGet = true)]
+    [Consumes("application/json")]
+    [ApiController]
+    public class ReturnTypeChekingController: ControllerBase
+    {
+        // Return Type
+        [HttpGet]
+        // for swagger endpoint documentation
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get(string x)
+        {
+            if (string.IsNullOrEmpty(x))
+            {
+                return BadRequest("null parameter x found");
+            }
+            return Ok($"{x}");
+        }
+    }
+}
